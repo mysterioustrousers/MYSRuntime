af0531dac2214b4f94fe099cac4e4c1995868234
Adam Kirk <atomkirk@gmail.com>
2013-11-02 09:54:28 -0600

    Initial Commit

---
+++ MYSRuntime/MYSRuntime.h
@@ -0,0 +1,13 @@
+//
+//  MYSRuntime.h
+//  MYSRuntime
+//
+//  Created by Adam Kirk on 11/2/13.
+//  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYSRuntime : NSObject
+
+@end
---
+++ MYSRuntime/MYSRuntime.m
@@ -0,0 +1,13 @@
+//
+//  MYSRuntime.m
+//  MYSRuntime
+//
+//  Created by Adam Kirk on 11/2/13.
+//  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
+//
+
+#import "MYSRuntime.h"
+
+@implementation MYSRuntime
+
+@end
